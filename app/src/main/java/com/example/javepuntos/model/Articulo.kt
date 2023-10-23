d1b9cd4961b055011dbeb4ec1439f51cd8805128
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2023-10-22 20:59:42 -0500

    Pantalla menu inicial

---
+++ app/src/main/java/com/example/javepuntos/model/Articulo.kt
@@ -0,0 +1,15 @@
+package com.example.javepuntos.model
+import com.google.gson.annotations.SerializedName
+
+data class Articulo(
+    @SerializedName("id_articulo") val idArticulo: Int,
+    @SerializedName("id_dpto") val idDpto: Int,
+    @SerializedName("id_seccion") val idSeccion: Int,
+    @SerializedName("descripcion") val descripcion: String,
+    @SerializedName("proveedor") val proveedor: String?,
+    @SerializedName("unidadmedida") val unidadMedida: String?,
+    @SerializedName("medidareferencia") val medidaReferencia: String?,
+    @SerializedName("tipoarticulo") val tipoArticulo: String?,
+    @SerializedName("stock") val stock: Int?,
+    @SerializedName("precio") val precio: Float
+)
