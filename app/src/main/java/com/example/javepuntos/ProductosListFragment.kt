d8cad5c5b171a62a2cc0371c149c208bd53c0c75
Fabian Zapata <fabianzapata2001@hotmail.com>
2023-10-29 02:12:03 -0500

    Fragmento ListaProductos

--- app/src/main/java/com/example/javepuntos/ProductosListFragment.kt
+++ app/src/main/java/com/example/javepuntos/ProductosListFragment.kt
@@ -1,8 +1,11 @@
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
@@ -12,18 +15,22 @@ import com.example.javepuntos.model.Producto
 import org.json.JSONArray
 import org.json.JSONException
 
-class ProductosList : AppCompatActivity() {
+class ProductosListFragment : Fragment() {
     private val productList = ArrayList<Producto>()
     private lateinit var listView: ListView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_productos_list)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_productos_list2, container, false)
 
-        listView = findViewById(R.id.productListView)
+        listView = view.findViewById(R.id.productListView)
 
         // Llama a la función para cargar los datos de LoopBack
         loadProductData()
+
+        return view
     }
 
     private fun loadProductData() {
@@ -48,17 +55,17 @@ class ProductosList : AppCompatActivity() {
                 }
 
                 // Crear un adaptador para el ListView
-                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, productList)
+                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, productList)
 
                 // Asignar el adaptador al ListView
                 listView.adapter = adapter
             },
             Response.ErrorListener { error ->
-                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Error: ${error.message}", Toast.LENGTH_SHORT).show()
             }
         )
 
         // Agregar la solicitud a la cola de solicitudes
-        Volley.newRequestQueue(this).add(jsonArrayRequest)
+        Volley.newRequestQueue(requireContext()).add(jsonArrayRequest)
     }
 }
