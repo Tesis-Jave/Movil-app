d1eeac9321567cfa7d52cb0b385e1efbfbd854b9
German Andrés Velasco Bossa <germana-velasco@javeriana.edu.co>
2023-10-31 23:52:03 -0500

    puntos de perfil, saldo

--- app/src/main/java/com/example/javepuntos/Perfil_Puntos.kt
+++ app/src/main/java/com/example/javepuntos/Perfil_Puntos.kt
@@ -7,6 +7,7 @@ import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.compose.ui.res.integerArrayResource
 import com.example.javepuntos.databinding.ActivityPerfilPuntosBinding
 import com.example.javepuntos.model.TokenResponse
 import com.example.javepuntos.model.cliente
@@ -28,21 +29,16 @@ class Perfil_Puntos : AppCompatActivity() {
 
         binding = ActivityPerfilPuntosBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        findViewById<AppCompatImageButton>(R.id.imageButton7).setOnClickListener {
-            finish()
-        }
 
-        val gson =Gson()
-        val tokenResponse: TokenResponse = gson.fromJson(intent.getStringExtra("response_data"), object : TypeToken<TokenResponse>() {}.type)
+        val token1 = intent.getStringExtra("response_data")
 
-        val idCliente = tokenResponse.id
-        println("Id de cliente")
-        println(idCliente)
+        val idCliente = intent.getStringExtra("idCliente")
 
         val sharedPreferences = getSharedPreferences("MiAppPreferences", Context.MODE_PRIVATE)
         val token = sharedPreferences.getString("TOKEN_KEY", null)
 
-        val url = "$BASE_URL/clientes?id=$idCliente"
+        val url = "${BASE_URL}/clientes/${idCliente}"
+        println(url)
 
         // Realizar la solicitud para obtener la lista de departamentos
         val client = OkHttpClient()
@@ -60,10 +56,21 @@ class Perfil_Puntos : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
+                    val gson = Gson()
                     val responseData = response.body()?.string()
+                    println(responseData)
                     // Procesa la respuesta exitosa aquí
-                    val cliente : cliente = gson.fromJson(responseData,object : TypeToken<cliente>(){}.type)
-                    binding.nombreTextView.setText(cliente.nombrecliente)
+                    val clienteE: cliente = gson.fromJson(responseData, object : TypeToken<cliente>() {}.type)
+
+                    if (clienteE != null) {
+                        runOnUiThread {
+                            binding.nombreTextView.text = clienteE.nombrecliente
+
+                        }
+                    } else {
+                        println("No se encontraron clientes en la respuesta JSON")
+                    }
+
                 } else {
                     println("Error")
                     Toast.makeText(
@@ -74,6 +81,68 @@ class Perfil_Puntos : AppCompatActivity() {
                 }
             }
         })
+
+        val url2 = "${BASE_URL}/clientes/${idCliente}/balance"
+
+        // Realizar la solicitud para obtener la lista de departamentos
+        val client2 = OkHttpClient()
+        val request2 = Request.Builder()
+            .url(url2)
+            .header("Authorization", "Bearer $token")
+            .build()
+
+        client2.newCall(request2).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Manejo de errores en caso de que la solicitud falle
+                println(e)
+                finish()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val gson = Gson()
+                    val responseData = response.body()?.string()
+                    println(responseData)
+                    // Procesa la respuesta exitosa aquí
+                    val saldo: String = gson.fromJson(responseData, object : TypeToken<String>() {}.type)
+
+                    if (saldo != null) {
+                        runOnUiThread {
+                            binding.saldoTextView.text ="$saldo puntos"
+
+                        }
+                    } else {
+                        println("No se encontraron clientes en la respuesta JSON")
+                    }
+
+                } else {
+                    println("Error")
+                    Toast.makeText(
+                        applicationContext,
+                        "Error al obtener el saldo del cliente",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+
+        binding.transferir.setOnClickListener {
+
+            intent = Intent(this,Transferir::class.java)
+            intent.putExtra("response_data",token)
+            intent.putExtra("id_cliente",idCliente)
+            startActivity(intent)
+
+        }
+
+        binding.historial.setOnClickListener {
+            
+            intent = Intent(this,Historial::class.java)
+            intent.putExtra("response_data",token)
+            intent.putExtra("id_cliente",idCliente)
+            startActivity(intent)
+
+        }
 
     }
 
